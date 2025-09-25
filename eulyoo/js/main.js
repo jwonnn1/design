0e8e43b31c9cbf7f3467802699fa1661877a6d62
jwonnn1 <jwonnn1@gmail.com>
2025-09-25 18:01:51 +0900

    250925 을유문화사

--- eulyoo/js/main.js
+++ eulyoo/js/main.js
@@ -69,4 +69,20 @@ $(document).ready(function(){
         $('header .gnb .gnb_open').show()
     })
 
+    const book_swiper = new Swiper('.book .swiper', { /* 팝업을 감싼는 요소의 class명 */
+        slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+        breakpoints: {
+            769: {    /* 768px 이상일때 적용 */
+                slidesPerView: 'auto',    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+                spaceBetween: 24,
+            },
+        },
+
+        navigation: {
+            nextEl: '.book .btn_next',
+            prevEl: '.book .btn_prev',
+        },
+    });
+
 })//맨끝
