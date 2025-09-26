beca3131529df1bcf7625c876b543c7146de23f1
jwonnn1 <jwonnn1@gmail.com>
2025-09-26 13:05:45 +0900

    250926 평가

--- eulyoo/js/main.js
+++ eulyoo/js/main.js
@@ -85,4 +85,19 @@ $(document).ready(function(){
         },
     });
 
+    const story_swiper = new Swiper('.story .swiper', { /* 팝업을 감싼는 요소의 class명 */
+        slidesPerView: 2, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+        breakpoints: {
+            640: {    /* 640px 이상일때 적용 */
+                slidesPerView: 3,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+                spaceBetween: 20,
+            },
+        },
+        navigation: {
+            nextEl: '.btn_next',
+            prevEl: '.btn_prev',
+        }
+    });
+
 })//맨끝
