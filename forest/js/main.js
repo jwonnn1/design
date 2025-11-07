8308b29805199a824c7eb11881dd419bf48cda74
jwonnn1 <jwonnn1@gmail.com>
2025-11-07 16:02:31 +0900

    251107

--- forest/js/main.js
+++ forest/js/main.js
@@ -33,7 +33,7 @@ $(document).ready(function(){
 		},
 
 		responsiveWidth: 769, /* fullpage를 적용시키지 않을 모바일 사이즈 */
-        responsiveHeight: 500
+        responsiveHeight: 600
 	});
 	//fullpage
 
@@ -76,6 +76,27 @@ $(document).ready(function(){
 
 	/* 숫자 카운팅 */
 	
+
+	const news_swiper = new Swiper('.news .swiper', { /* 팝업을 감싼는 요소의 class명 */
+		slidesPerView: 1, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+		spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+		breakpoints: {
+			471: {    /* 640px 이상일때 적용 */
+				slidesPerView: 2,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+				spaceBetween: 16,
+			},
+			769: {    /* 640px 이상일때 적용 */
+				slidesPerView: 3,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+				spaceBetween: 24,
+			},
+			1025: {    /* 640px 이상일때 적용 */
+				slidesPerView: 4,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+				spaceBetween: 24,
+			},
+		},
+		//centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+		
+	});
 	
 
 })//맨끝
